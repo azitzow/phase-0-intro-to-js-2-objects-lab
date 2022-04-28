987011eec9daff169421f66943043bef7d8dd1ce
Ana Zitzow <anasempai89@gmail.com>
2022-04-27 17:00:01 -0700

    commit tree 4e8655121db99480bb1b9ae202d78ca64dba2fe8

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+  name: 'Brian',
+  streetAddress: '633 Glenwood Dr'
+};
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+  let newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+  employee[key] = value;
+  return employee;
+}
+
+const deleteFromEmployeeByKey = (employee, key) => {
+  let newEmployee = { ...employee}
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+  delete employee[key];
+  return employee;
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -4,7 +4,6 @@ describe('employees', function() {
       for (const key in employee) {
         delete employee[key];
       }
-
       employee.name = 'Sam';
     });
 
